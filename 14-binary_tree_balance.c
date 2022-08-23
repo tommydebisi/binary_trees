08377ee514c1b7f0917c253ff52c6000a6e03634
tommydebisi <tommydebisi@gmail.com>
2022-08-23 17:09:08 +0100

    :construction: tasks 14 and 15 added

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor
+ * of a binary tree
+ *
+ * @tree: pointer to the root node of the
+ * tree to measure the balance factor
+ *
+ * Return: the balance factor of the tree or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	/* balance factor = left tree height - right tree height */
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree:  pointer to the root node of the tree to measure the height
+ *
+ * Return: the height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (get_maxHeight(tree) - 1);
+}
+
+/**
+ * get_maxHeight - gets the maximum height of a tree
+ *
+ * @tree: pointer to the root node of the tree
+ * Return: The maximum height of a tree
+ */
+size_t get_maxHeight(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+	if (!tree)
+		return (0);
+
+	left = 1 + get_maxHeight(tree->left);
+	right = 1 + get_maxHeight(tree->right);
+
+	if (left < right)
+		return (right);
+	else
+		return (left);
+}
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ *
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is full or 0 is not full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+	if (!tree)
+		return (0);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	if (!left && !right) /* just starting out or not */
+	{
+		if ((!tree->left && !tree->right) || (tree->left && tree->right))
+			return (1);
+		return (0);
+	}
+	else if (!left || !right)
+		return (0);
+	return(1);	/* two children are present */
+}
