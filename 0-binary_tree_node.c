adae6c678a59ba7d4b2aed0535ad9f0e08a6dbac
tommydebisi <tommydebisi@gmail.com>
2022-08-23 09:26:43 +0100

    :seedling: convert to tabs

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,16 +10,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = !parent ? NULL : parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = !parent ? NULL : parent;
 
-    return (new_node);
+	return (new_node);
 }
