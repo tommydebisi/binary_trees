8a90ac6db6608d8a4e72915c3fc7a65e0d3aaae6
tommydebisi <tommydebisi@gmail.com>
2022-08-23 11:08:36 +0100

    :rotating_light: betty linter fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-		binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,17 +10,17 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-		binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = value;
-	new_node->right = NULL;
+	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->parent = parent;
 
