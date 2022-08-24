aa60a0fe0ff323a71f21d77b6ce8f0ffddf94896
tommydebisi <tommydebisi@gmail.com>
2022-08-24 04:15:54 +0100

    :rotating_light: betty errors fixed

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,6 +14,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left == node)
 		return (node->parent->right);
-	else
-		return (node->parent->left);
+	return (node->parent->left);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle: finds the uncle of a node
+ * binary_tree_uncle - finds the uncle of a node
  *
  * @node: pointer to the node to find the uncle
  * Return: a pointer to the uncle node or NULL if
@@ -17,8 +17,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
-	else
-	{
-		return (node->parent->parent->left);
-	}
+	return (node->parent->parent->left);
 }
