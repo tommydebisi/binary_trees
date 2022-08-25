5cd671bbd446321ba308e8dd0556737ecc5a7c5f
tommydebisi <tommydebisi@gmail.com>
2022-08-25 13:46:49 +0100

    :rotating_light: betty fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,27 +9,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return (get_maxHeight(tree) - 1);
-}
-
-/**
- * get_maxHeight - gets the maximum height of a tree
- *
- * @tree: pointer to the root node of the tree
- * Return: The maximum height of a tree
- */
-size_t get_maxHeight(const binary_tree_t *tree)
-{
 	size_t left = 0, right = 0;
+
 	if (!tree)
 		return (0);
 
-	left = 1 + get_maxHeight(tree->left);
-	right = 1 + get_maxHeight(tree->right);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
 
 	if (left < right)
 		return (right);
 	else
 		return (left);
 }
-
