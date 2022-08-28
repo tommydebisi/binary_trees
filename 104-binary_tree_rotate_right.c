1eba12a9d6771086e49edcfec6db171a1d03d3cf
tommydebisi <tommydebisi@gmail.com>
2022-08-28 19:30:24 +0100

    :seedling: changed pivot right parent

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -11,24 +11,12 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	if (!tree || !tree->left)
 		return (tree);
 
-	if (tree->parent)
-	{
-		if (tree->parent->left == tree)
-		{
-			tree->parent->left = tree->left;
-			tree->left->parent = tree->parent;
-		}
-		else
-		{
-			tree->parent->right = tree->left;
-			tree->left->parent = tree->parent;
-		}
-	}
-	else
-		tree->left->parent = NULL;
+	tree->left->parent = tree->parent;
 
 	tree->parent = tree->left;
 	tree->left = tree->left->right;
+	if (tree->left)
+		tree->left->parent = tree;
 	tree->parent->right = tree;
 	return (tree->parent);
 }
